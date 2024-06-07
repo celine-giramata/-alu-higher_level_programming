4bbf994a1d90c950bb460d6b6212e94a06d87ad9
celine-giramata <c.giramata@alustudent.com>
2024-06-07 08:41:24 +0000

    task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,21 +1,11 @@
 #!/usr/bin/node
 
-t fs = require('fs');
+const request = require('fs');
 
-function readFile (filePath) {
-  fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-      console.error(`An error occurred while reading the file: ${err}`);
-    } else {
-      console.log(data);
-    }
-  });
-}
+const filePath = process.argv[2];
 
-// Check if the file path is provided as an argument
-if (process.argv.length > 2) {
-  const filePath = process.argv[2];
-  readFile(filePath);
-} else {
-  console.log('Please provide the file path as an argument.');
-}i
+request.readFile(filePath, function (err, data) {
+  if (err) console.log(err);
+  const content = data;
+  console.log(content.toString());
+});
