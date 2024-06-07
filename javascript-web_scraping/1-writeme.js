868c268d1b505279855498e1831d4fd604a8b175
celine-giramata <c.giramata@alustudent.com>
2024-06-07 09:29:43 +0000

    task 1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,14 +2,18 @@
 
 const fs = require('fs');
 
-const filePath = process.argv[2];
+function writeFile (filePath, content) {
+  fs.writeFile(filePath, content, 'utf-8', (err) => {
+    if (err) {
+      console.error(`An error occurred while writing to the file: ${err}`);
+    }
+  });
+}
 
-const data = process.argv[3];
-
-fs.writeFile(filePath, data, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.readFileSync(filePath, 'utf8');
-  }
-});
+if (process.argv.length > 3) {
+  const filePath = process.argv[2];
+  const content = process.argv[3];
+  writeFile(filePath, content);
+} else {
+  console.log('provide the file path and content as arguments.');
+}
